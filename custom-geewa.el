91b63b5a32e844af08c8353fc18cef2cd7622043
wvi <vilibald@wvi.cz>
2014-06-25 10:43:53 +0200

    Remove backup.

--- custom-geewa.el
+++ custom-geewa.el
@@ -3,11 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(column-number-mode t)
- '(show-paren-mode t)
- '(tabbar-separator (quote (0.5)))
- '(tool-bar-mode nil))
+ '(tab-width 4)
+ '(tabbar-separator (quote (0.5))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
