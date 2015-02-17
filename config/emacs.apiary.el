ac9edeea7d59205a018fcfe50a1ed5385851f29a
wvi <vilibald@apiary.com>
2015-02-17 08:40:17 +0100

    Apiary config.

---
+++ config/emacs.apiary.el
@@ -0,0 +1,54 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; Startup and Behavior Controls 
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'cl)
+
+(setq load-path (cons "~/emacs" load-path)) 
+(setq custom-file "~/emacs/custom.el")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; Machine Specific Configuration Section
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; go FULLSCREEN
+(defun toggle-fullscreen ()
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                         '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+                         '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+)
+
+(when (eq window-system 'x)
+  (toggle-fullscreen))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;; Load the real init
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'wvi-init)
+
+(defun wvi-cedet-hook ()
+  (setq ac-sources (append '(ac-source-semantic) ac-sources))
+  (local-set-key (kbd "RET") 'newline-and-indent)
+  (semantic-mode 1))
+(add-hook 'c-mode-common-hook 'wvi-cedet-hook)
+
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/ecb")
+(setq-default ecb-tip-of-the-day nil)
+(require 'ecb)
+
+;; TRAMP
+(require 'tramp)
+
+;; GYP
+(setq load-path (cons "~/src/gyp/tools/emacs" load-path))
+(require 'gyp)
